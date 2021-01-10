d206aecfa323f676151ee2cee48d2f9622b2d085
ilvif666 <ilvif666@gmail.com>
2021-01-10 16:57:50 +0300

    in for variable holds each value of args array and says "hello @string@"

---
+++ HelloArray.java
@@ -0,0 +1,10 @@
+public class HelloArray {
+  public static void main(String[] args) {
+    for (String s: args) {
+      System.out.println("Hello, "+ s + "!");
+    }
+  }
+}
+/*
+s в цикле for принимает все значения массива args
+*/
