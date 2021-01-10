c428b15172106cb97fc31e6733d96232db2ace2b
ilvif666 <ilvif666@gmail.com>
2021-01-10 22:51:38 +0300

    -модификатор static у name и say() +конструкторк который задает пеменной name изначальное значение +метод setName(String) для установки имени итого: в main создает объект оператором new и вызываются его методы

--- something something/Hello.java
+++ something something/Hello.java
@@ -1,12 +1,28 @@
 public class Hello {
-  private static String name;
+  private String name;
+
+  public Hello() {
+    name = "peace";
+  }
+
+  public void setName(String s) {
+    name = s;
+  }
+
   public static void main(String[] args) {
+    Hello h = new Hello();
     for (String s : args) {
-      name = s;
-      say();
+      h.setName(s);
+      h.say();
     }
   }
-  public static void say() {
-    System.out.println("Hello, "+ name + "!");
+  public void say() {
+    System.out.println("Hello, " + name + "!");
   }
 }
+/*
+в предыдущем примере не было ООП. Убираем static у переменной name и метода say()
+Добавляем конструктор, который присваивает переменной name начальное значение
+а также метод setName(String) для установки имени.
+Теперь в методе main создается объект с помощью оператора new и вызываются его методы
+*/
