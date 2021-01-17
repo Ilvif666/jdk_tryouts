8b76608cc0726d336ebcdfea135a424cb3282a6b
ilvif666 <ilvif666@gmail.com>
2021-01-17 05:07:26 +0300

    - something something

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 /** класс для приветствия типа "Hello" */
 public class Hello {
-  protected String name;
+    protected String name;
   /** Если имя было  не задано,
     то будет использоваться слово "мир". */
   public Hello() {
